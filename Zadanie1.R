2d642fe0a8357e35680a3b8cc8ecfa184cb2909e
bdziak <bdziakjank@gmail.com>
2022-12-03 21:21:15 +0100

    Zadanie 3 - policzony współczynnik Paersona

--- Zadanie1.R
+++ Zadanie1.R
@@ -24,3 +24,23 @@ v_2<-90.
 
 v_sr= 2.*v_1*v_2/(v_1+v_2)
 print(v_sr)
+
+# Zadanie 3
+#3. Utwórz funkcję obliczającą współczynnik korelacji r Pearsona dla 2 wektorów o tej samej długości.
+#sWczytaj dane plik dane.csv i oblicz współczynnik dla wagi i wzrostu. W komentarzu napisz co oznacza wynik.
+
+funkcja_corr<-function(x1,x2){
+  wspolcz_paers=cor(x1,y1)
+  return(wspolcz_paers)
+}
+
+
+daneDF<-read.csv("dane.csv",sep=";")
+#mean(daneDF$wiek)
+mean(daneDF$wzrost)
+mean(daneDF$waga)
+x1<-daneDF$wzrost
+y1<-daneDF$waga
+print(funkcja_corr(x1,y1))
+# Wartość 0.9793459 współczynnika korelacji Paersona
+#między wzrostem i wagą oznacza, że jest to bardzo silna korelacja
